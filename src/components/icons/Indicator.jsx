00dff98a7073edd226e854bc709aac94efabcbdb
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-07 20:56:05 +0330

    feat: add icon components

---
+++ src/components/icons/Indicator.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+const SvgIndicator = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" {...props}>
+    <path d="m5.396 28.424.303-.316A40.6 40.6 0 0 0 17 0v83.059A41.57 41.57 0 0 0 5.407 54.26c-6.933-7.215-6.938-18.614-.011-25.835Z" />
+  </svg>
+);
+export default SvgIndicator;
---
+++ src/components/icons/index.jsx
@@ -0,0 +1,5 @@
+export { default as BlackBg } from "./BlackBg";
+export { default as GreenBg } from "./GreenBg";
+export { default as Indicator } from "./Indicator";
+export { default as TopFooterBg } from "./TopFooterBg";
+export { default as Workio } from "./Workio";
