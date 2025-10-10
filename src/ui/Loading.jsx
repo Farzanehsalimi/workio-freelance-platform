c3bd763dc4946531fbc2c89ac1cd6c77af2c6231
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-10 23:45:22 +0330

    feat: add LoadingSpinner component

---
+++ src/ui/Loading.jsx
@@ -0,0 +1,21 @@
+import { ThreeDots } from "react-loader-spinner";
+
+function Loading({ width = 75, height = 40 }) {
+  return (
+    <div>
+      <ThreeDots
+        width={width}
+        height={height}
+        radius={9}
+        color="rgb(var(--color-primary-600))"
+        wrapperStyle={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+        visible={true}
+      />
+    </div>
+  );
+}
+
+export default Loading;
