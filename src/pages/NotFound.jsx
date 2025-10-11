5faf1a015ce41de50f9285605a8e8f7f931a072e
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-12 00:24:47 +0330

    feat: add NotFound page and custom useMoveBack hook

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,25 @@
+import { HiArrowRight } from "react-icons/hi";
+import useMoveBack from "../hooks/useMoveBack";
+
+function NotFound() {
+  const moveBack = useMoveBack();
+  return (
+    <div className="h-screen bg-secondary-0">
+      <div className="container xl:max-w-screen-xl">
+        <div className="sm:max-w-sm pt-10">
+          <div>
+            <h1 className="text-lg sm:text-2xl font-bold text-secondary-700 mb-8">
+              صفحه ای که دنبالش بودید، پیدا نشد!
+            </h1>
+            <button onClick={moveBack} className="flex items-center gap-x-2">
+              <HiArrowRight className="w-6 h-6 text-primary-500" />
+              <span className="text-lg sm:text-xl text-primary-500">برگشت</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
