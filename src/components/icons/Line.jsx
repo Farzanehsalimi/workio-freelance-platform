8ebf8e67ed13d9877368c06c95dd86b9e7edae63
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-26 15:33:12 +0330

    feat: add new icon component to components directory

---
+++ src/components/icons/Line.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+const SvgLine = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 3 36"
+    {...props}
+  >
+    <path fill="#F5B91E" d="M3 0H0v36h3z" />
+  </svg>
+);
+export default SvgLine;
--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -1,5 +1,1 @@
-export { default as BlackBg } from "./BlackBg";
-export { default as GreenBg } from "./GreenBg";
-export { default as Indicator } from "./Indicator";
-export { default as TopFooterBg } from "./TopFooterBg";
-export { default as Workio } from "./Workio";
+export { default as Line } from "./Line";
