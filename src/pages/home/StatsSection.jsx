cd543fc19aa5eb4d6e2b1c7de85e8a72806b759e
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-26 17:12:53 +0330

    style: add responsive borders and spacing to StatsSection

--- src/pages/home/StatsSection.jsx
+++ src/pages/home/StatsSection.jsx
@@ -1,40 +1,43 @@
 import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
 
 function StatsSection() {
+  const smStyle =
+    "border sm:border-none border-primary-400 border-b-4 rounded-2xl py-1.5 sm:py-0 px-1 sm:px-0";
+
   return (
-    <div className="hidden sm:flex sm:flex-row justify-around mt-14 sm:mt-24 md:mt-32">
-      <div className="flex flex-col items-center">
-        <span className="sm:text-7xl md:text-8xl lg:text-9xl font-bold text-green-600">
+    <div className="flex sm:flex-row gap-1 justify-around mt-14 sm:mt-24 md:mt-32 px-4 max-w-xs sm:max-w-none mx-auto">
+      <div className={`flex flex-col items-center ${smStyle}`}>
+        <span className="text-xl sm:text-7xl md:text-8xl font-bold text-green-600">
           {toPersianNumbersWithComma(95)}
           <span className="sm:text-4xl md:text-5xl lg:text-7xl align-super">
             %
           </span>
         </span>
-        <span className="sm:text-xl md:text-2xl lg:text-3xl text-secondary-800 font-bold">
+        <span className="text-xs sm:text-xl md:text-2xl text-secondary-800 font-bold">
           رضایت کاربران
         </span>
       </div>
-      <div className="flex flex-col items-center">
-        <span className="sm:text-7xl md:text-8xl lg:text-9xl font-bold text-green-600">
+      <div className={`flex flex-col items-center ${smStyle}`}>
+        <span className="text-xl sm:text-7xl md:text-8xl font-bold text-green-600">
           <span className="sm:text-4xl md:text-5xl lg:text-7xl align-super">
             +
           </span>
           {toPersianNumbersWithComma(4)}
-          <span className="sm:text-2xl md: lg:text-6xl">میلیون</span>
+          <span className="text-sm sm:text-2xl lg:text-4xl">میلیون</span>
         </span>
-        <span className="sm:text-xl md:text-2xl lg:text-3xl text-secondary-800 font-bold">
+        <span className="text-xs sm:text-xl md:text-2xl text-secondary-800 font-bold">
           کاربر فعال
         </span>
       </div>
-      <div className="flex flex-col items-center">
-        <span className="sm:text-7xl md:text-8xl lg:text-9xl font-bold text-green-600">
+      <div className={`flex flex-col items-center ${smStyle}`}>
+        <span className="text-xl sm:text-7xl md:text-8xl font-bold text-green-600">
           <span className="sm:text-4xl md:text-5xl lg:text-7xl align-super">
             +
           </span>
           {toPersianNumbersWithComma(30)}
-          <span className="sm:text-2xl md: lg:text-6xl">هزار</span>
+          <span className="text-sm sm:text-2xl lg:text-4xl">هزار</span>
         </span>
-        <span className="sm:text-xl md:text-2xl lg:text-3xl text-secondary-800 font-bold">
+        <span className="text-xs sm:text-xl md:text-2xl text-secondary-800 font-bold">
           شرکت تایید شده
         </span>
       </div>
