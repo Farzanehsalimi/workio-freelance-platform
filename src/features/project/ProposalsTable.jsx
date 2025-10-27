f1c46f885cf4df0402d49113f60ae5df8c9ee072
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-27 19:46:16 +0330

    feat(project): add ProposalsTable component to display proposal list with actions

---
+++ src/features/project/ProposalsTable.jsx
@@ -0,0 +1,32 @@
+import Empty from "../../ui/Empty";
+import Table from "../../ui/Table";
+import ProposalRow from "./ProposalRow";
+
+function ProposalsTable({ proposals }) {
+  // early return
+  if (!proposals.length) return <Empty resourceName="پروپوزالی" />;
+  return (
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>فریلنسر</th>
+        <th>توضیحات</th>
+        <th>زمان تحویل</th>
+        <th>هزینه</th>
+        <th>وضعیت</th>
+        <th>عملیات</th>
+      </Table.Header>
+      <Table.Body>
+        {proposals.map((proposal, index) => (
+          <ProposalRow
+            key={proposal._id ?? index}
+            proposal={proposal}
+            index={index}
+          />
+        ))}
+      </Table.Body>
+    </Table>
+  );
+}
+
+export default ProposalsTable;
