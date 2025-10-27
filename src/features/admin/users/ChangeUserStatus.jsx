ed2a8b6b0081336f948903cee4379b9040b9492b
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-27 08:02:10 +0330

    feat(admin): add ChangeUserStatus form with react-hook-form and mutation handling

---
+++ src/features/admin/users/ChangeUserStatus.jsx
@@ -0,0 +1,65 @@
+import { useForm } from "react-hook-form";
+import { useQueryClient } from "@tanstack/react-query";
+import RHFSelect from "../../../ui/RHFSelect";
+import Loading from "../../../ui/Loading";
+import useChangeUserStatus from "./useChangeUserStatus";
+
+const options = [
+  {
+    label: "رد شده",
+    value: 0,
+  },
+  {
+    label: "در انتظار تایید",
+    value: 1,
+  },
+  {
+    label: "تایید شده",
+    value: 2,
+  },
+];
+
+function ChangeUserStatus({ userId, onClose }) {
+  const { register, handleSubmit, setValue } = useForm();
+  const { changeUserStatus, isUpdating } = useChangeUserStatus();
+  const queryClient = useQueryClient();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    changeUserStatus(
+      { userId, data },
+      {
+        onSuccess: () => {
+          onClose();
+          queryClient.invalidateQueries({ queryKey: ["users"] });
+        },
+      }
+    );
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="">
+        <RHFSelect
+          name="status"
+          label="تغییر وضعیت"
+          register={register}
+          options={options}
+          setValue={setValue}
+          required
+        />
+        <div className="!mt-8">
+          {isUpdating ? (
+            <Loading />
+          ) : (
+            <button type="submit" className="btn btn--primary w-full">
+              تایید
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ChangeUserStatus;
