2d24c1d167786a604d1e49e6b63d1654a021e958
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-27 08:15:00 +0330

    feat(admin): add Users page with UsersTable

---
+++ src/pages/Users.jsx
@@ -0,0 +1,12 @@
+import UsersTable from "../features/admin/users/UsersTable";
+
+function Users() {
+  return (
+    <div>
+      <h1 className="text-secondary-700 text-2xl font-black mb-8">کاربران</h1>
+      <UsersTable />
+    </div>
+  );
+}
+
+export default Users;
