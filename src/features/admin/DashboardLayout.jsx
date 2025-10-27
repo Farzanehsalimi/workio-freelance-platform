a0102bca0ca8c6a5e6d464f642dd0c064cde701a
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-27 07:56:31 +0330

    feat(admin): implement DashboardLayout with data fetching for users, projects, and proposals

---
+++ src/features/admin/DashboardLayout.jsx
@@ -0,0 +1,26 @@
+import Loading from "../../ui/Loading";
+import DashboardHeader from "../../ui/DashboardHeader";
+import useProposals from "../Proposals/useProposals";
+import useProjects from "../../hooks/useProjects";
+import useUsers from "./useUsers";
+import Stats from "./Stats";
+
+function DashboardLayout() {
+  const { isLoading: isLoading1, proposals } = useProposals();
+  const { isLoading: isLoading2, projects } = useProjects();
+  const { isLoading: isLoading3, users } = useUsers();
+  if (isLoading1 || isLoading2 || isLoading3) return <Loading />;
+
+  return (
+    <div>
+      <DashboardHeader />
+      <Stats
+        proposals={proposals.length}
+        users={users.length}
+        projects={projects.length}
+      />
+    </div>
+  );
+}
+
+export default DashboardLayout;
