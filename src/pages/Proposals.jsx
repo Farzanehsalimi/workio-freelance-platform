a208453ef597c608ca806b572f5f812dd3613de7
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-27 20:56:51 +0330

    feat(proposals): create Proposals page with proposals table

---
+++ src/pages/Proposals.jsx
@@ -0,0 +1,14 @@
+import ProposalTable from "../features/Proposals/ProposalTable";
+
+function Proposals() {
+  return (
+    <div>
+      <h1 className="text-2xl font-black text-secondary-700 mb-8">
+        لیست پروپوزال ها
+      </h1>
+      <ProposalTable />
+    </div>
+  );
+}
+
+export default Proposals;
