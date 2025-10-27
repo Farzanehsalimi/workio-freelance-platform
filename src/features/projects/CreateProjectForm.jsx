fbcd23c83e816145dbf1366ce04eded99b0923ad
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-27 19:58:07 +0330

    add(projects): implement CreateProjectForm component with create and edit project functionality

---
+++ src/features/projects/CreateProjectForm.jsx
@@ -0,0 +1,155 @@
+import { useState } from "react";
+import TextField from "../../ui/TextField";
+import { useForm } from "react-hook-form";
+import RHFSelect from "../../ui/RHFSelect";
+import { TagsInput } from "react-tag-input-component";
+import DatePickerField from "../../ui/DatePickerField";
+import useCategories from "../../hooks/useCategories";
+import useCreateProject from "./useCreateProject";
+import Loading from "../../ui/Loading";
+import useEditProject from "./useEditProject";
+function CreateProjectForm({ onClose, projectToEdit = {} }) {
+  const { _id: editId } = projectToEdit;
+  const isEditSession = Boolean(editId);
+
+  const {
+    title,
+    description,
+    budget,
+    category,
+    deadline,
+    tags: prevTags,
+  } = projectToEdit;
+
+  let editValues = {};
+
+  if (isEditSession) {
+    editValues = {
+      title,
+      description,
+      budget,
+      category: category._id,
+      deadline,
+    };
+  }
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+    setValue,
+  } = useForm({ defaultValues: editValues });
+
+  const [tags, setTags] = useState(prevTags || []);
+  const [date, setDate] = useState(new Date(deadline || ""));
+  const { categories } = useCategories();
+  const { isCreating, createProject } = useCreateProject();
+  const { editProject, isEditing } = useEditProject();
+
+  const onSubmit = (data) => {
+    const newProject = {
+      ...data,
+      deadline: new Date(date).toISOString(),
+      tags,
+    };
+    console.log({
+      ...data,
+      deadline: new Date(date).toISOString(),
+      tags,
+    });
+    if (isEditSession) {
+      editProject(
+        { id: editId, newProject },
+        {
+          onSuccess: () => {
+            onClose();
+            reset();
+          },
+        }
+      );
+    } else {
+      createProject(newProject, {
+        onSuccess: () => {
+          onClose();
+          reset();
+        },
+      });
+    }
+  };
+
+  return (
+    <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="عنوان پروژه"
+        name="title"
+        register={register}
+        required
+        validationSchema={{
+          required: "عنوان ضروری است",
+          minLength: {
+            value: 10,
+            message: "طول عنوان نامعتبر است",
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="توضیحات"
+        name="description"
+        register={register}
+        required
+        validationSchema={{
+          required: "توضیحات ضروری است",
+          minLength: {
+            value: 10,
+            message: "حداقل 15 کاراکتر را وارد کنید",
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="بودجه"
+        name="budget"
+        type="number"
+        register={register}
+        required
+        validationSchema={{
+          required: "بودجه ضروری است",
+          minLength: {
+            value: 5,
+            message: "طول عنوان نامعتبر است",
+          },
+        }}
+        errors={errors}
+      />
+      <RHFSelect
+        label="دسته بندی"
+        placeholder="دسته بندی پروژه ی خود را انتخاب کنید"
+        required
+        name="category"
+        register={register}
+        options={categories}
+        setValue={setValue}
+      />
+      <div>
+        <label className="nb-2 block text-secondary-700">تگ ها</label>
+        <TagsInput value={tags} onChange={setTags} name="tags" />
+      </div>
+      <div className="">
+        <DatePickerField date={date} setDate={setDate} label="ددلاین" />
+      </div>
+      <div className="!mt-8">
+        {isCreating ? (
+          <Loading />
+        ) : (
+          <button type="submit" className="btn btn--primary w-full">
+            تایید
+          </button>
+        )}
+      </div>
+    </form>
+  );
+}
+
+export default CreateProjectForm;
