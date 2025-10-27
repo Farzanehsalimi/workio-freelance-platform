3cd8aac6b6da140deb6552199bc8b72968271e2b
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2025-10-27 07:57:01 +0330

    feat(admin): add Stats component to display user, project, and proposal metrics

---
+++ src/features/admin/Stats.jsx
@@ -0,0 +1,31 @@
+import { HiOutlineInboxArrowDown, HiOutlineFolderOpen } from "react-icons/hi2";
+import { UserGroupIcon } from "@heroicons/react/24/outline";
+import Stat from "../../ui/Stat";
+
+function Stats({ proposals, users, projects }) {
+  return (
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-x-2 lg:gap-x-8">
+      <Stat
+        color="blue"
+        title="کاربران"
+        value={users}
+        icon={<UserGroupIcon className="w-16 h-16 md:w-20 md:h-20" />}
+      />
+      <Stat
+        color="yellow"
+        title="درخواست ها"
+        value={proposals}
+        icon={<HiOutlineInboxArrowDown className="w-16 h-16 md:w-20 md:h-20" />}
+      />
+
+      <Stat
+        color="primary"
+        title="پروژه ها"
+        value={projects}
+        icon={<HiOutlineFolderOpen className="w-16 h-16 md:w-20 md:h-20" />}
+      />
+    </div>
+  );
+}
+
+export default Stats;
